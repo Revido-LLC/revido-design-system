d0a006a08fdc68af2d0a2a5d9c4c6d7954979332
Lucas Santos <lucas@revido.io>
2026-05-26 09:08:59 -0300

    chore: scaffold monorepo root with npm workspaces and turborepo

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+  ignorePatterns: ['dist/', 'node_modules/', 'storybook-static/'],
+}
